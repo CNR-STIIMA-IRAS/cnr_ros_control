2fc96d19e95bcb8e280199f5fd28b2615bcb9a2e
Manuel Beschi <manuel.beschi@unibs.it>
2022-01-17 16:42:45 +0100

    add none priority for saturation

--- cnr_controller_interfaces/cnr_controller_interface/include/cnr_controller_interface/internal/cnr_joint_command_controller_interface_impl.h
+++ cnr_controller_interfaces/cnr_controller_interface/include/cnr_controller_interface/internal/cnr_joint_command_controller_interface_impl.h
@@ -223,12 +223,16 @@ inline bool JointCommandController<H,T>::exitUpdate()
 
     // ============================== ==============================
     auto saturated_qd = nominal_qd;
-    if(rosdyn::saturateSpeed(this->chain(), saturated_qd, m_last_target.qd(), m_last_target.q(),
-                               this->m_sampling_period, m_max_velocity_multiplier, true, &report))
+
+    if (m_priority != NONE)
     {
-      print_report = true;
-      m_target.q()  = m_last_target.q() + saturated_qd * this->m_dt.toSec();
-      m_target.qd() = saturated_qd;
+      if(rosdyn::saturateSpeed(this->chain(), saturated_qd, m_last_target.qd(), m_last_target.q(),
+                                 this->m_sampling_period, m_max_velocity_multiplier, true, &report))
+      {
+        print_report = true;
+        m_target.q()  = m_last_target.q() + saturated_qd * this->m_dt.toSec();
+        m_target.qd() = saturated_qd;
+      }
     }
 
     m_last_target.copy(m_target, m_target.ONLY_JOINT);
